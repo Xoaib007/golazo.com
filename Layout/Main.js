c8b242e937cd9a2c2f2ec2154f914ed175d17973
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-25 09:57:13 +0100

    header

---
+++ Layout/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Components/Shared/Footer';
+import Header from '../Components/Shared/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            This is home
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
