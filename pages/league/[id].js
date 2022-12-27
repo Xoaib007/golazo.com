ca0148db7bbf10aa590448264c818bf48c53e694
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-27 22:42:06 +0100

    api fetch

--- pages/league/[id].js
+++ pages/league/[id].js
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import React from 'react';
+import { Badge, Button, Mask, Table } from 'react-daisyui';
 
-const singleLeague = ({ league, match }) => {
+const singleLeague = ({ league, match, standing, topScorers }) => {
     return (
         <div className='min-h-screen mx-20 mt-20'>
             <div className='flex'>
@@ -12,7 +13,7 @@ const singleLeague = ({ league, match }) => {
                 </div>
             </div>
 
-            <div className='mt-20'>
+            <div className='mt-20 flex'>
 
                 {/* Matches section */}
                 <div className='w-1/2 rounded-lg bg-white'>
@@ -25,7 +26,7 @@ const singleLeague = ({ league, match }) => {
                                 <div key={match.fixture.id}>
                                     {
                                         match.fixture.status.short === 'FT' ?
-                                            <div className='flex justify-between px-20 p-4 border-b-2 border-gray-700'>
+                                            <div className='flex justify-between px-10 p-4 border-b-2 border-gray-700'>
                                                 <div className='flex w-1/2'>
                                                     <Image src={match.teams.home.logo} width={50} height={50} alt='' />
                                                     <p className='pt-3'>{match.teams.home.name}</p>
@@ -56,8 +57,8 @@ const singleLeague = ({ league, match }) => {
                                                 </div>
 
                                                 <div className='w-32 h-12 bg-gray-200'>
-                                                    <p className='ml-2'>{match.fixture.date.slice(0,10)}</p>
-                                                    <p className='ml-8'>{match.fixture.date.slice(11,16)}</p>
+                                                    <p className='ml-2'>{match.fixture.date.slice(0, 10)}</p>
+                                                    <p className='ml-8'>{match.fixture.date.slice(11, 16)}</p>
                                                 </div>
 
                                                 <div className='flex w-1/2 justify-end'>
@@ -69,6 +70,98 @@ const singleLeague = ({ league, match }) => {
                                 </div>
                             )
                         }
+                    </div>
+                </div>
+
+                <div>
+                    {/* Standings Section */}
+                    <div className='ml-10'>
+                        <div className='h-16 w-full bg-[#243F85]'>
+                            <p className='text-white text-3xl ml-[40%] font-semibold pt-4'>Standings</p>
+                        </div>
+                        <div className='overflow-x-auto'>
+                            <Table className="rounded-box">
+                                <Table.Head>
+                                    <span />
+                                    <span />
+                                    <span>Team</span>
+                                    <span>pts.</span>
+                                    <span>G</span>
+                                    <span>W</span>
+                                    <span>L</span>
+                                    <span>D</span>
+                                    <span>GS</span>
+                                    <span>GC</span>
+                                    <span>GD</span>
+                                    <span>Form</span>
+                                    <span />
+                                </Table.Head>
+
+                                <Table.Body>
+                                    {
+                                        standing?.response[0]?.league?.standings[0]?.map(team =>
+                                            <Table.Row key={team.rank}>
+                                                <div>{team.rank}</div>
+                                                <div><Image src={team.team.logo} width={280} height={280} alt='' /></div>
+                                                <div>{team.team.name}</div>
+                                                <div>{team.points}</div>
+                                                <div>{team.all.played}</div>
+                                                <div>{team.all.win}</div>
+                                                <div>{team.all.lose}</div>
+                                                <div>{team.all.draw}</div>
+                                                <div>{team.all.goals.for}</div>
+                                                <div>{team.all.goals.against}</div>
+                                                <div>{team.goalsDiff}</div>
+                                                <div>{team.form}</div>
+                                                <div></div>
+                                            </Table.Row>
+                                        )
+                                    }
+                                </Table.Body>
+                            </Table>
+                        </div>
+                    </div>
+
+                    {/* Top Scorer */}
+                    <div className='ml-10 mt-20'>
+                        <div className='h-16 w-full bg-[#243F85]'>
+                            <p className='text-white text-3xl ml-[40%] font-semibold pt-4'>Top Scorers</p>
+                        </div>
+                        <div className='overflow-x-auto'>
+                            <Table className="rounded-box">
+                                <Table.Head>
+                                    <span />
+                                    <span />
+                                    <span>Name</span>
+                                    <span>Age</span>
+                                    <span>Country</span>
+                                    <span>Team</span>
+                                    <span>Played</span>
+                                    <span>Goals</span>
+                                    <span>Assists</span>
+                                    <span />
+                                </Table.Head>
+
+                                <Table.Body>
+                                    {
+                                        topScorers?.response?.map((topScorer, i )=>
+                                            <Table.Row key={topScorer.player.id}>
+                                                <div>{i}</div>
+                                                <div><Image src={topScorer.player.photo} width={70} height={70} alt='' /></div>
+                                                <div>{topScorer.player.name}</div>
+                                                <div>{topScorer.player.age}</div>
+                                                <div>{topScorer.player.nationality}</div>
+                                                <div><Image src={topScorer.statistics[0].team.logo} width={50} height={50} alt='' /></div>
+                                                <div>{topScorer.statistics[0].games.appearences}</div>
+                                                <div>{topScorer.statistics[0].goals.total}</div>
+                                                <div>{topScorer.statistics[0].goals.assists}</div>
+                                                <div></div>
+                                            </Table.Row>
+                                        )
+                                    }
+                                </Table.Body>
+                            </Table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -89,8 +182,6 @@ const options = {
     }
 };
 
-// https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39
-
 export const getServerSideProps = async (context) => {
     const leagueRes = await fetch('https://api-football-v1.p.rapidapi.com/v3/leagues?id=' + context.params.id, options)
     const league = await leagueRes.json();
@@ -98,10 +189,18 @@ export const getServerSideProps = async (context) => {
     const matchRes = await fetch(`https://api-football-v1.p.rapidapi.com/v3/fixtures?league=${context.params.id}&season=2022`, options)
     const match = await matchRes.json();
 
+    const standingRes = await fetch(`https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=${context.params.id}`, options)
+    const standing = await standingRes.json();
+
+    const topScorersRes = await fetch(`https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=${context.params.id}&season=2022`, options)
+    const topScorers = await topScorersRes.json();
+
     return {
         props: {
             league,
-            match
+            match,
+            standing,
+            topScorers
         }
     }
 }
