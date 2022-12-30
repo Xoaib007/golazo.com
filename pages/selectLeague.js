109f9a89c2c8857d002d648573ca4e4e765c321a
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-31 00:49:23 +0100

    ..

--- pages/selectLeague.js
+++ pages/selectLeague.js
@@ -5,6 +5,7 @@ import laliga from '../Assets/LA_LIGA.png'
 import serieA from '../Assets/SERIE_A.png'
 import bundesliga from '../Assets/bundesliga.png'
 import ucl from '../Assets/CL.png'
+import europa from '../Assets/europa.png'
 import Link from 'next/link';
 
 const SelectLeague = () => {
@@ -34,18 +35,23 @@ const SelectLeague = () => {
             title: 'UEFA Champions League',
             logo: ucl,
         },
+        {
+            id: 3,
+            title: 'UEFA Europa League',
+            logo: europa,
+        },
     ]
 
     return (
         <div className='min-h-screen bg-sky-100'>
             <p className='text-3xl w-fit text-gray-600 font-bold text-center pt-10 border-b-8 border-sky-700 mx-auto px-4 pb-4 '>Choose a league</p>
-            <div className='grid grid-cols-5 mx-56 '>
+            <div className='grid grid-cols-6 mx-56 '>
                 {
                     leagues.map(league =>
                         <Link key={league.id} as={`/match/${league.id}`} href='/match/[id]'>
                             <div className='flex flex-col items-center mt-20'>
-                                <Image className='rounded-full shadow-xl' src={league.logo} width={140} height={140} alt='' />
-                                <p className='text-xl font-semibold mt-3'>{league.title}</p>
+                                <Image className='rounded-full shadow-xl' src={league.logo} width={110} height={110} alt='' />
+                                <p className='text-xl text-center font-semibold mt-3'>{league.title}</p>
                             </div>
                         </Link>
                     )
