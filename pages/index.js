1d073a2228f4988f85d7b729602c190392e6513f
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-30 05:28:36 +0100

    fixed

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Home({ countries }) {
   const todayDate =  yyyy + '-' + mm + '-' + dd ;
 
   return (
-    // <>
-    //   <Meta />
-    //   <div className='bg-cover1 min-h-screen pt-10'>
-    //     <p className='text-white text-5xl font-bold mt-[13%] ml-20'>Predict matches and <br />win <span className=''>amazing</span> gifts....</p>
+    <>
+      <Meta />
+      <div className='bg-cover1 min-h-screen pt-10'>
+        {/* <p className='text-white text-5xl font-bold mt-[13%] ml-20'>Predict matches and <br />win <span className=''>amazing</span> gifts....</p> */}
 
-    //     <Link href='/matches?date=[date]' as={`/matches?date=${todayDate}`} className="btn1 black">Watch more...</Link>
-    //   </div>
+        <Link href='/matches?date=[date]' as={`/matches?date=${todayDate}`} className="btn1 black">Watch more...</Link>
+      </div>
 
-    // </>
+    </>
   )
 }
