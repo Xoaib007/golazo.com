943a2edbca94a4d2d8689afe90ccbb81edf333be
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-30 07:29:22 +0100

    ..

--- pages/matches.js
+++ pages/matches.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Matches = () => {
     return (
         <div className='min-h-screen'>
-<p>Hiiii there</p>
-<p>Welcome here</p>
+        <p>everythings ok?</p>
         </div>
     );
 };
