3aa5507acaa61aa54cbd8c30085bedd5f079733e
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-01-01 16:55:45 +0100

    ..

--- pages/matchDetails/[details].js
+++ pages/matchDetails/[details].js
@@ -8,7 +8,7 @@ import vs from '../../Assets/vs.png'
 const matchDetails = ({ h2h, homeTeam, awayTeam, homePlayers, awayPlayers, homeCoach, awayCoach }) => {
 
 
-    const date = format(new Date(), 'dd MMM yyyy')
+    const date = format(new Date(), 'dd MMM yyyy');
 
     return (
         <div className='min-h-screen bg-sky-100 py-20'>
@@ -39,17 +39,58 @@ const matchDetails = ({ h2h, homeTeam, awayTeam, homePlayers, awayPlayers, homeC
                 </div>
 
                 <div className='overflow-x-auto'>
-                    <Table>
-                        <Table.Body>
-                            <Table.Row>
-                                <Image className='rounded-full' src={homeCoach?.response[0]?.photo} width={75} height={75} alt='' />
-                                <p>{homeCoach?.response[0].name}</p>
-                                <span className='text-2xl'>I</span>
-                                <p>{awayCoach?.response[0].name}</p>
-                                <Image className='rounded-full' src={awayCoach?.response[0]?.photo} width={75} height={75} alt='' />
-                            </Table.Row>
-                        </Table.Body>
-                    </Table>
+                    <div className='flex justify-center'>
+                        <Table>
+                            <Table.Body>
+                                <Table.Row>
+                                    <Image className='rounded-full' src={homeCoach?.response[0]?.photo} width={75} height={75} alt='' />
+                                    <p>{homeCoach?.response[0].name}</p>
+                                    <span className='text-2xl'>I</span>
+                                    <p>{awayCoach?.response[0].name}</p>
+                                    <Image className='rounded-full' src={awayCoach?.response[0]?.photo} width={75} height={75} alt='' />
+                                </Table.Row>
+                            </Table.Body>
+                        </Table>
+                    </div>
+
+                    <div>
+                        <p className='text-3xl border-b-4 border-gray-500 mt-32'>History</p>
+                        {
+                            h2h?.response?.map(match =>
+                                <div className='px-20 mt-10 h-32 w-[640px] rounded-xl bg-white mb-10 mx-5' key={match.fixture.id}>
+
+                                    <div className='w-fit text-center mx-auto'>
+                                        <p>{match.league.name}</p>
+                                        <p>{match.fixture.date.slice(0, 10)}</p>
+                                    </div>
+
+                                    <div className='flex justify-between items-center'>
+                                        <div className='flex w-1/2'>
+                                            <Image src={match.teams.home.logo} width={50} height={50} alt='' />
+                                            <p className={match.teams.home.name.length > 9 ? 'text-sm pt-5' : 'pt-3'}>{match.teams.home.name}</p>
+                                        </div>
+
+                                        <div className=''>
+                                            <form className='flex'>
+                                                <div className='h-12 w-8 border-2 bg-white border-gray-300  rounded-lg flex justify-center items-center text-2xl font-bold'>
+                                                    <p>{match.goals.home}</p>
+                                                </div>
+                                                <p className='mx-2 mt-2'>-</p>
+                                                <div className='h-12 w-8 border-2 bg-white border-gray-300 rounded-lg flex justify-center items-center text-2xl font-bold'>
+                                                    <p>{match.goals.away}</p>
+                                                </div>
+                                            </form>
+                                        </div>
+
+                                        <div className='flex w-1/2 justify-end'>
+                                            <p className={match.teams.away.name.length > 9 ? 'text-sm pt-5 pr-2' : 'pt-3 pr-2'}>{match.teams.away.name}</p>
+                                            <Image src={match.teams.away.logo} width={50} height={50} alt='' />
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
                 </div>
 
                 <div className='overflow-x-auto'>
