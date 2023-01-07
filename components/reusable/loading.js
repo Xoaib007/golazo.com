cd9bdd176d4c29b060ad61ceca5f5fa9029b9b4d
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2023-01-07 12:50:17 +0000

    loader

---
+++ components/reusable/loading.js
@@ -0,0 +1,9 @@
+const Loading = ({ players }) => {
+    return (
+        <div>
+            <iframe src="https://embed.lottiefiles.com/animation/12710"></iframe>
+        </div>
+    )
+}
+
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,45 @@
 import Main from '../Layout/Main'
 import '../styles/globals.css'
 import { Comfortaa } from '@next/font/google'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const font= Comfortaa({
-  subsets:['latin'],
-  weight:['400', '700']
+const font = Comfortaa({
+  subsets: ['latin'],
+  weight: ['400', '700']
 })
+
+function Loading() {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+    const handleComplete = (url) => (url === router.asPath) && setTimeout(() => { setLoading(false) }, 5000);
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
+  return loading && (<div className='spinner-wrapper'>
+    <div className="spinner"></div></div>)
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <div className={font.className}>
-    <Main>
-      <Component {...pageProps} />
-    </Main>
+      <Main>
+        <Loading />
+        <Component {...pageProps} />
+      </Main>
     </div>
   )
 }
